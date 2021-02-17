780343fbcd1474f863e5d2810d33f06fa2f3a35c
Dan <dtan9988@gmail.com>
2021-02-16 21:37:56 -0800

    tweak key error for list

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,9 +4,8 @@ import DayListItem from './DayListItem';
 export default function DayList(props) {
   const dayData = props.days.map((day, index) => {
     return (
-      <li>      
+      <li key={index}>
         <DayListItem
-          key={index}
           name={day.name}
           spots={day.spots}
           selected={day.name === props.day}
