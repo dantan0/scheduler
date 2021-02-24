4081adaf44007cdfd87c1c6dad47b37d64df2fd8
Dan <dtan9988@gmail.com>
2021-02-23 18:05:05 -0800

    implement application integration tests and fix daylistitem warning

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,14 +4,13 @@ import DayListItem from './DayListItem';
 export default function DayList(props) {
   const dayData = props.days.map((day, index) => {
     return (
-      <li key={index}>
-        <DayListItem
-          name={day.name}
-          spots={day.spots}
-          selected={day.name === props.day}
-          setDay={props.setDay}
-        />
-      </li>
+      <DayListItem
+        key={index}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
     )
   })
 
